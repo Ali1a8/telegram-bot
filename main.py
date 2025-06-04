d67448be351a34dd0491a90f8b85f1d1c18babe0
Ali1a8 <almajd99ali@gmail.com>
2025-06-04 08:44:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import telebot
 import os
-TOKEN = os.getenv("TOKEN")
-
-import os
 
 TOKEN = os.environ.get("BOT_TOKEN")
 bot = telebot.TeleBot(TOKEN)
 
+# الرسالة الترحيبية الجديدة
+welcome_message = "مرحبًا بك! أنا بوت المجد، كيف يمكنني مساعدتك؟"
+
+# يرد على /start
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "أهلاً بك! هذا بوت تجريبي.")
+    bot.reply_to(message, welcome_message)
 
+# يرد على أي رسالة بنفس الترحيب
 @bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
+def reply_all(message):
+    bot.reply_to(message, welcome_message)
 
 bot.infinity_polling()
